ede2f98b18209d8b45a492602eea9f5377f47477
Tzach Ovadia <tzach@stuccomedia.com>
2019-04-22 18:39:09 +0300

    feat: send comment via gmail

--- src/modules/gmail.ts
+++ src/modules/gmail.ts
@@ -2,7 +2,6 @@ import * as vscode from 'vscode';
 
 import { ActionComment } from '../models/action-comment';
 import { Config } from '../config';
-import { readFileSync } from 'fs';
 
 export class Gmail {
 
@@ -11,23 +10,22 @@ export class Gmail {
     }
 
     private async sendUsingGmail(comment: ActionComment) {
-        // TODO: add full comment + code snippet to body
         const body = await this.getSnippet(comment.uri, comment.position, 5);
         const url = `https://mail.google.com/mail/?view=cm&fs=1&su=${comment.label}&body=${body}`;
         return vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(url));
     }
 
     private async getSnippet(resource: vscode.Uri, startAt: number, numberOfLines: number) {
-        // let content = readFileSync(filename, 'utf8');
         const editor = await vscode.window.showTextDocument(resource);
         const pos = editor.document.positionAt(startAt);
         const startLine = Math.max(pos.line - 2, 0);
         const endLine = Math.min(editor.document.lineCount, pos.line + numberOfLines);
         let content = editor.document.getText(new vscode.Range(startLine, 0, endLine, 0));
-
-        content = content + `\n\n${this.config.scheme}://TzachOvadia.todo-list/view?file=${encodeURIComponent(resource.fsPath)}#${startAt})`;
+        content = content.trim();
+        const file = `${encodeURIComponent(resource.fsPath)}:${pos.line + 1}:${pos.character + 1}`;
+        const line = '-'.repeat(file.length);
+        content = `Snippet:\n${line}\n${content}\n${line}\n${file}`;
         content = encodeURI(content.replace(/ /g, '+').replace(/\n/g, '%0A'));
-        console.log('newLines', content);
         return content;
     }
 }
